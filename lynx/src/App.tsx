2c40b5e80072b3b873dccae3f6ead3608fa920e3
Brendan Viscomi <brendanv@users.noreply.github.com>
2024-08-08 02:49:58 +0000

    Add dark/light mode provider and toggle

--- lynx/src/App.tsx
+++ lynx/src/App.tsx
@@ -4,32 +4,35 @@ import Home from "@/components/pages/Home";
 import Login from "@/components/pages/Login";
 import AuthWrapper from "@/components/AuthWrapper";
 import { PocketBaseProvider } from "@/hooks/usePocketBase";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const App = () => {
   return (
-    <PocketBaseProvider>
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-            </ul>
-          </nav>
+    <ThemeProvider>
+      <PocketBaseProvider>
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+              </ul>
+            </nav>
 
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route element={<AuthWrapper />}>
-              <Route path="/" element={<Home />} />
-            </Route>
-          </Routes>
-        </div>
-      </Router>
-    </PocketBaseProvider>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route element={<AuthWrapper />}>
+                <Route path="/" element={<Home />} />
+              </Route>
+            </Routes>
+          </div>
+        </Router>
+      </PocketBaseProvider>
+    </ThemeProvider>
   );
 };
 
--- lynx/src/components/pages/Home.tsx
+++ lynx/src/components/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { usePocketBase } from "@/hooks/usePocketBase";
+import { DarkModeToggle } from "../DarkModeToggle";
 
 const Home = () => {
   const { user } = usePocketBase();
@@ -7,6 +8,7 @@ const Home = () => {
   return (
     <div>
       <h1>Lynx</h1>
+      <DarkModeToggle />
       {user && <p>Welcome, {user.username || user.email}!</p>}
     </div>
   );
