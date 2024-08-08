d17003892836e95f1a2a4e2fe6a85fb5eca22c40
Brendan Viscomi <brendanv@users.noreply.github.com>
2024-08-08 01:42:55 +0000

    shadcn setup

--- lynx/src/App.tsx
+++ lynx/src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button"
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <div>
+        <Button>hello world</Button>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
