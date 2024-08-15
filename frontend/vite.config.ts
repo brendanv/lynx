257ce3b29f4bfddd2c97a43916c9b005b34afe1c
Brendan Viscomi <brendanv@users.noreply.github.com>
2024-08-14 21:54:57 -0400

    Configure CI for frontend tests (#2)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,15 @@
+/// <reference types="vitest" />
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: "./tests/setup.js",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
