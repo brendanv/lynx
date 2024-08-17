a410b6de2106c222e1721144b73ee174ce35ad96
Brendan Viscomi <brendanv@users.noreply.github.com>
2024-08-17 17:03:47 +0000

    Add unread indicator

--- frontend/src/components/LinkCard.tsx
+++ frontend/src/components/LinkCard.tsx
@@ -9,15 +9,20 @@ import {
 import { Link } from "react-router-dom";
 import URLS from "@/lib/urls";
 
-const LinkCard = ({ link }: { link: FeedLink }) => {
+const LinkCard: React.FC<{ link: FeedLink }> = ({ link }) => {
+  const isUnread = link.last_viewed_at === null;
+
   return (
     <Card className="flex items-start p-4 gap-4 mb-4">
-      <div className="flex-shrink-0">
+      <div className="flex-shrink-0 relative">
         <img
           src={link.header_image_url || "/img/lynx_placeholder.png"}
           alt={link.title || ""}
           className="object-cover w-12 h-12 md:w-16 md:h-16 lg:w-20 lg:h-20 rounded-lg"
         />
+        {isUnread && (
+          <div className="absolute -top-1 -left-1 w-4 h-4 bg-blue-500 rounded-full border-2 border-white"></div>
+        )}
       </div>
       <div className="flex-grow min-w-0">
         <Link to={URLS.LINK_VIEWER(link.id)}>
