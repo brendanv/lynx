b5cf5e6cca321747045c73be1f755919002670ed
Brendan Viscomi <brendanv@users.noreply.github.com>
2024-08-24 02:38:45 +0000

    revamp login page

--- frontend/src/components/pages/Login.tsx
+++ frontend/src/components/pages/Login.tsx
@@ -2,21 +2,11 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { usePocketBase } from "@/hooks/usePocketBase";
 import { useTheme } from "@/components/ThemeProvider";
-import {
-  Alert,
-  AlertDescription,
-  AlertTitle,
-} from "@/components/ui/alert";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { usePageTitle } from "@/hooks/usePageTitle";
+import { Label } from "@/components/ui/label";
 
 const Login = () => {
   const [username, setUsername] = useState("");
@@ -25,7 +15,7 @@ const Login = () => {
   const navigate = useNavigate();
   const { pb } = usePocketBase();
   const { theme } = useTheme();
-  usePageTitle("Login")
+  usePageTitle("Login");
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
@@ -43,53 +33,82 @@ const Login = () => {
   };
 
   return (
-    <main className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px8">
-      <Card className="max-w-md w-full p-8 space-y-8 shadow-lg bg-background rounded-lg">
-        <CardHeader>
-          <CardTitle>Lynx</CardTitle>
-          <CardDescription>Sign in to your account</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-            <input type="hidden" name="remember" value="true" />
-            <div className="space-y-4">
-              <Input
-                id="username"
-                name="username"
-                type="text"
-                autoComplete="username"
-                required
-                placeholder="Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                className={`${theme === "dark" ? "bg-gray-700 text-white" : "bg-gray-100 text-gray-900"}`}
-              />
-              <Input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className={`${theme === "dark" ? "bg-gray-700 text-white" : "bg-gray-100 text-gray-900"}`}
-              />
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Lynx</h1>
+            <p className="text-balance text-muted-foreground">
+              Enter your username below to login to your account
+            </p>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="grid gap-4">
+              {error && (
+                <Alert variant="destructive">
+                  <AlertTitle>Login Failed</AlertTitle>
+                  <AlertDescription>{error}</AlertDescription>
+                </Alert>
+              )}
+              <div className="grid gap-2">
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  id="username"
+                  name="username"
+                  type="text"
+                  autoComplete="username"
+                  required
+                  placeholder="lynx"
+                  tabIndex={1}
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="/forgot-password"
+                    className="ml-auto inline-block text-sm underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  placeholder="Password"
+                  tabIndex={2}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
             </div>
-
-            {error && (
-              <Alert variant="destructive">
-                <AlertTitle>Login Failed</AlertTitle>
-                <AlertDescription>{error}</AlertDescription>
-              </Alert>
-            )}
-            <Button type="submit" className="w-full">
-              Sign in
-            </Button>
           </form>
-        </CardContent>
-      </Card>
-    </main>
+          {/* <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <a href="#" className="underline">
+              Sign up
+            </a>
+          </div> */}
+        </div>
+      </div>
+      <div className="hidden bg-muted lg:block">
+        <img
+          src="/img/lynx_login.png"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.5] dark:grayscale"
+        />
+      </div>
+    </div>
   );
 };
 
