db3d07bf07fc4bd2ed443a6e09b40f1336e43ad2
Brendan Viscomi <brendanv@users.noreply.github.com>
2024-09-22 18:00:05 +0000

    Fix link viewer header z-index issue

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -40,7 +40,7 @@ const Header = () => {
   const sideBarCurrentClassName = "text-foreground hover:text-foreground";
 
   return (
-    <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+    <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 z-50">
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link
           to={URLS.HOME}
