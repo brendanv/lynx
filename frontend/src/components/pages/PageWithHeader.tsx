7a267772b7a2dfb72d831e3d007f750565f8458e
Brendan Viscomi <brendanv@users.noreply.github.com>
2024-09-26 23:08:17 -0400

    Update color scheme (#53)
    https://tinte.railly.dev/shadcn -> light "Urban Chic", dark "DoctorWebsiteTheme"

--- frontend/src/components/pages/PageWithHeader.tsx
+++ frontend/src/components/pages/PageWithHeader.tsx
@@ -6,7 +6,7 @@ const PageWithHeader: React.FC<{ children: React.ReactNode }> = ({
 }) => (
   <div className="flex min-h-screen w-full flex-col">
     <Header />
-    <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+    <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted p-4 md:gap-8 md:p-10">
       {children}
     </main>
   </div>
@@ -17,7 +17,7 @@ export const FullBleedPageWithHeader: React.FC<{
 }> = ({ children }) => (
   <div className="flex min-h-screen w-full flex-col">
     <Header />
-    <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40">
+    <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted">
       {children}
     </main>
   </div>
